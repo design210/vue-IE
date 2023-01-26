b9f9b734cdedaf4697198b36306de37406e33503
kyle <design210@nate.com>
2023-01-26 15:48:53 +0900

    session storage

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,15 @@ import Vuex from 'vuex';
 
 // common
 import common from '@/store/modules/common/common.js';
-
+import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 export default new Vuex.Store({
 	modules: {
 		common,
 	},
+	plugins: [
+		createPersistedState({
+			storage: window.sessionStorage,
+		}),
+	],
 });
