955987f2cf4ecbac5e42a3d0a0a9c2244adf9ad6
kyle <design210@nate.com>
2023-01-26 10:52:25 +0900

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const ansiRegex = require('ansi-regex')
 
 module.exports = {
-    transpileDependencies: [ansiRegex]
+    transpileDependencies: [
+      ({onlyFirst = false} = {}) => {
+          const pattern = [
+              '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+              '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+          ].join('|');
+      
+          return new RegExp(pattern, onlyFirst ? undefined : 'g');
+      },
+      'vuetify'
+    ]
 }
