e7c3305340cfaecb1573c7b4a5a406cd3bb6ecf4
kyle <design210@nate.com>
2023-01-26 12:38:13 +0900

    router & scss

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            redirect: '/main'
+        },
+        {
+            path: '/main',
+            component: () => import("@/views/Main.vue"),
+        },
+        {
+            path: '/list',
+            component: () => import("@/views/List.vue"),
+        },
+    ]
+});
+
+export default router;
---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>main</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+</style>
