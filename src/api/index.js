15811c7f4bf4785f092ff0c4d53d28a6a80e10ed
kyle <design210@nate.com>
2023-01-26 12:54:03 +0900

    axios

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const baseUrl = "https://dapi.kakao.com";
+const key = '2747cc77d6c23300d9fb150e23b1b9df';
+function kakaoBookSearchApi(params) {
+  return axios
+    .get(`${baseUrl}/v3/search/book`, {
+      headers: {
+        Authorization: `KakaoAK ${key}`,
+      },
+      params,
+    })
+    .then(res => res)
+}
+export { kakaoBookSearchApi };
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>main</div>
+    <div>
+        <div>main</div>
+        <div @click="searchBooks">api 호출</div>
+        <div>{{ res }}</div>
+    </div>
+    
 </template>
 
 <script>
+import { kakaoBookSearchApi } from "../api/index";
 export default {
   name: 'Main',
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      res:null
+    };
+  },
+  methods: {
+    async searchBooks() {
+        const response = await kakaoBookSearchApi({ query: '똥' });
+        this.res = response;
+    }
+  }
 };
 </script>
 <style>
